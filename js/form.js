f180de47d35d64de7e380ad31cba437e59885793
Eunice Travassos <77119496+EuniceTravassos@users.noreply.github.com>
2021-04-20 10:18:50 -0300

    Add pagina nutricionista

---
+++ js/form.js
@@ -0,0 +1,102 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function (event) {
+	event.preventDefault();
+	var form = document.querySelector("#form-adiciona")
+	var paciente = obtemPacienteDoFormulario(form);
+    var erros = validaPaciente (paciente);
+	if(erros.length > 0){
+        exibeMensagensDeErro(erros);
+        return;
+    }
+
+    adicionaPacienteNaTabela(paciente);
+
+    form.reset();
+    var mensagensErros = document.querySelector("#mensagens-erro");
+    mensagensErros.innerHTML = "";
+
+});
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+
+    erros.forEach(function(erros){
+        var li = document.createElement("li");
+        li.textContent = erros;
+        ul.appendChild(li);
+    });
+}
+
+function obtemPacienteDoFormulario(form){
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
+
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+	pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+	pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+	pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+	pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
+}
+
+function validaPaciente(paciente){
+
+    var erros = [];
+
+    if(paciente.nome.length == 0){
+        erros.push("O campo nome não pode ser em branco!");
+    }
+
+    if(!validaPeso(paciente.peso)){
+    erros.push("O campo peso é inválido!");
+    }
+
+    if(!validaAltura(paciente.altura)){
+    erros.push("O campo altura é inválida!");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("O campo gordura não pode ficar em branco!");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("O campo peso não pode ficar em branco!");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("O campo altura não pode ficar em branco!");
+    }
+
+    return erros;
+
+ }
---
+++ js/remover-paciente.js
@@ -0,0 +1,27 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick",function(event){
+   
+    event.target.parentNode.classList.add("fadeOut");
+   
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500);
+
+
+
+    //var alvoEvento = event.target;
+    //var paiDoAlvo = alvoEvento.parentNode; 
+    //paiDoAlvo.remove();
+   
+
+});
+
+//pacientes.forEach(function(paciente){
+//    paciente.addEventListener("dblclick", function(){
+ //       console.log("Fui clicado com duplo clique")
+ //       this.remove();
+//    });
+//});
